66e88356ebded860fd11a324ce45a6641bfb3c42
Yusuf Jamal <yusufjamal1372@gmail.com>
2022-06-12 17:20:24 +0530

    insigficant changes

--- src/bot.py
+++ src/bot.py
@@ -4,4 +4,12 @@ import nextcord
 from nextcord.ext import commands
 
 class RelBot(commands.Bot):
+
+    def __init__(self, command_prefix=..., help_command=..., description=None, **options):
+        super().__init__(command_prefix, help_command, description, **options)
+        
     
+    def is_ready(self) -> bool:
+        super().is_ready()
+        print("We're logged in!")
+        
--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import bot
+import json
+import os
+
+env_json = open(os.path.join("..", "env.json"))
+TOKEN = json.load(env_json)
 
 if __name__ == "__main__":
-    bot.RelBot()
+    bot.RelBot().
