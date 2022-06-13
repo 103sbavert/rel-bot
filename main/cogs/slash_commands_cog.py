d54c2542ed119f9ca5f2070e8e2ee99c6936a6c0
ShaunB21 <99273244+ShaunB21@users.noreply.github.com>
2022-06-13 14:14:03 +0100

    Category selection menu

--- main/cogs/slash_commands_cog.py
+++ main/cogs/slash_commands_cog.py
@@ -1,16 +1,32 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from discord.ui import Select, View
 
 
 class SlashCommandsCog(commands.Cog, name="SlashCommands"):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
+        
+    class MySelect(Select):
+        async def callback(self, interaction):
+            await interaction.edit_original_message(content="test")
     @app_commands.command(name="roles")
     async def roles(self, interaction: discord.Interaction):
-        await interaction.response.send_message("something", ephemeral=True)
+        select = SlashCommandsCog.MySelect(
+            placeholder = "Choose a catergory",
+            options=[
+                discord.SelectOption(label="English Level", emoji ="📖", description ="Roles that best represents your English level"),
+                discord.SelectOption(label="Languages", emoji ="📚", description ="Roles to display what languages you speak"),
+                discord.SelectOption(label="Other", emoji ="📃", description ="Other roles"),
+            ])
+        
+
+        
+        view = View()
+        view.add_item(select)
+        await interaction.response.send_message("Choose a catergory to select a role from", view=view, ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
