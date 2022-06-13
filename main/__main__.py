d811e4b96a1c5b9b34c9b890851fee0652f6ca25
Yusuf Jamal <yusufjamal1372@gmail.com>
2022-06-14 05:20:47 +0530

    add launch.json that fixes multiple bugs

--- main/__main__.py
+++ main/__main__.py
@@ -1,21 +1,25 @@
 import json
 import os
-import bot
+from main.bot import RelBot
 
 ENV_JSON_NAME = "env.json"
-ENV_JSON_PATH = os.path.join(os.getcwd(), "", ENV_JSON_NAME)
+ENV_JSON_PATH = os.path.join(os.getcwd(), ENV_JSON_NAME)
 
 
 def main():
-    env_json_file = open(ENV_JSON_PATH) if os.path.exists(ENV_JSON_PATH) else None
+    env_json_file = open(ENV_JSON_PATH, encoding="utf-8") if os.path.exists(ENV_JSON_PATH) else None
     json_str = env_json_file.read()
     json_obj = json.loads(json_str)
+
     token = json_obj["bot_token"]
     rel_id = json_obj["rel-id"]
-    print(rel_id)
+    
     env_json_file.close()
-    extension_list = ["cogs.slash_commands_cog"]
-    rel_bot = bot.RelBot(extension_list, token, rel_id)
+
+    extension_list = ["main.cogs.slash_commands_cog"]
+
+    rel_bot = RelBot(extension_list, token, rel_id)
+
     rel_bot.run()
 
 
