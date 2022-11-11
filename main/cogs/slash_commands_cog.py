9ac586efb91f980148226485423ad5940ec60374
ShaunB21 <99273244+ShaunB21@users.noreply.github.com>
2022-11-11 23:54:11 +0000

    Tidied up code

--- main/cogs/slash_commands_cog.py
+++ main/cogs/slash_commands_cog.py
@@ -1,22 +1,23 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from main.string_resources import StringResources
 from main.constants import fluency_levels, bot_role
 from main.views.roles_view.roles_view import RolesView
+from main.string_resources import StringResources
 
 class SlashCommandsCog(commands.Cog, name="SlashCommands"):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         
+    # Roles command that brings up the menu for choosing a role
     @app_commands.command(name=StringResources.roles_command_name, description=StringResources.roles_command_description)
     async def roles(self, interaction: discord.Interaction):
         view = RolesView()
         await interaction.response.send_message(StringResources.roles_command_message, view=view, ephemeral=True)
-        
+
     # Prune command that kicks members with no fluency role or bot role   
-    @app_commands.command(name="prune", description="Prune users who do not have a role")
+    @app_commands.command(name=StringResources.prune_command_name, description=StringResources.prune_command_description)
     @commands.has_role("Server Bot Team")
     async def prune(self, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True)
@@ -35,6 +36,7 @@ class SlashCommandsCog(commands.Cog, name="SlashCommands"):
                 if count >= 50:
                     break    
         await interaction.followup.send('Kicked ' + str(count) + ' member(s)')
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(SlashCommandsCog(bot))
--- main/string_resources.py
+++ main/string_resources.py
@@ -6,11 +6,17 @@ class StringResources:
     helper_messages_cog_name = "HelperMessages"
     fluency_roles_cog_name = "FluencyRoles"
     roles_command_name = "roles"
+    prune_command_name = "prune"
+    
     roles_command_description = "Select a role from the categories available!"
+    prune_command_description = "Prune users who do not have a role"
+    
     bot_description = ""
     roles_command_message = ""
+    
     helpChannel_message: str = f"Hi! To finish joining the server, you should go to the channel called <#{channels['english_level']}> and follow the instructions there!"
     talkerPing_message = "Great, now join a voice channel and wait!"
+    
     FLUENCY_MESSAGE_ID_KEY = "fluency_message_id"
     LANGUAGES_MESSAGE_ID_KEY = "languages_message_id"
     MISC_MESSAGE_ID_KEY = "misc_message_id"
